622fd60635a96dacbaa293f40f47c14052712b8c
Sam James <samjojames@gmail.com>
2025-06-06 08:24:45 +0100

    Add fuzzy search, display tags on screen

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from '../header/Header';
 import Task2 from '../task2/Task2';
 
@@ -10,14 +11,17 @@ import styles from './App.module.scss';
  * http://localhost:8000/api/cars - to return all cars
  * http://localhost:8000/api/cars?tag=ferrari - to return matching cars
  */
+const client = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <>
-      <Header />
-      <main className={styles.main}>
-        <Task2 />
-      </main>
+      <QueryClientProvider client={client}>
+        <Header />
+        <main className={styles.main}>
+          <Task2 />
+        </main>
+      </QueryClientProvider>
     </>
   );
 };
--- src/components/task1/Task1.tsx
+++ src/components/task1/Task1.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+import Fuse from 'fuse.js';
 
 // Please refer to task 1. Realtime search of readme.md
 const Task1: React.FC = () => {
-  return <div className="Task1">{/* Add search input here */}</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const { data } = useQuery({
+    queryKey: ['tags'],
+    queryFn: () => fetch('http://localhost:8000/api/tags').then((res) => res.json()),
+  });
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    const fuse = new Fuse<string>(data);
+    const result = fuse.search(searchTerm);
+
+    setTags(result.map(({ item }) => item));
+  };
+  return (
+    <div className="Task1">
+      {' '}
+      <input type="text" placeholder="Search tags..." value={searchTerm} onChange={handleSearch} />
+      {JSON.stringify(tags)}
+    </div>
+  );
 };
 
 export default Task1;
