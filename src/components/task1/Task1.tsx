1dcf71ca3fcbdb76b50d1988c7efe8f270220b0b
Sam James <samjojames@gmail.com>
2025-06-06 08:38:04 +0100

    add drop down list and basic styles

--- src/components/task1/Task1.tsx
+++ src/components/task1/Task1.tsx
@@ -2,11 +2,13 @@ import React, { ChangeEvent, useState } from 'react';
 
 import { useQuery } from '@tanstack/react-query';
 import Fuse from 'fuse.js';
+import styles from './Task1.module.scss';
 
 // Please refer to task 1. Realtime search of readme.md
 const Task1: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [tags, setTags] = useState([]);
+  const [isFocused, setIsFocused] = useState(false);
 
   const { data } = useQuery({
     queryKey: ['tags'],
@@ -17,14 +19,38 @@ const Task1: React.FC = () => {
     setSearchTerm(e.target.value);
     const fuse = new Fuse<string>(data);
     const result = fuse.search(searchTerm);
-
     setTags(result.map(({ item }) => item));
   };
+
   return (
-    <div className="Task1">
-      {' '}
-      <input type="text" placeholder="Search tags..." value={searchTerm} onChange={handleSearch} />
-      {JSON.stringify(tags)}
+    <div className={styles.container}>
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Search tags..."
+          value={searchTerm}
+          onChange={handleSearch}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setTimeout(() => setIsFocused(false), 100)}
+        />
+
+        {isFocused && (
+          <div className={styles.dropdown}>
+            {tags.map((tag) => (
+              <div
+                key={tag}
+                className={styles.suggestion}
+                onClick={() => {
+                  setSearchTerm(tag);
+                }}
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
