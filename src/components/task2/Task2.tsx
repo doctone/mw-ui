842e6e3e0494514c59014419c9918cc4228ce9c3
Sam James <samjojames@gmail.com>
2025-06-07 18:09:00 +0100

    add basic title on grid page

--- src/components/task2/Task2.tsx
+++ src/components/task2/Task2.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { SelectionContext } from '../app/hooks/useSelection';
 import { useQuery } from '@tanstack/react-query';
 import styles from './Task2.module.css';
+import SearchIcon from '../../assets/icons/search.svg?react';
 
 // Please refer to task 2. Realtime search results of readme.md
 const Task2: React.FC = () => {
@@ -14,20 +15,38 @@ const Task2: React.FC = () => {
     queryKey: ['cars', selection],
     queryFn: () => fetch(`http://localhost:8000/api/cars?${params}`).then((res) => res.json()),
   });
-  console.log(data);
+
+  if (!selection) {
+    return (
+      <div className={styles.title}>
+        <SearchIcon />
+        <h1>Use the search to find vehicles</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.grid}>
-      {data?.map((car) => {
-        return (
-          <div key={car.id} className={styles.imgContainer}>
-            <picture>
-              <source srcSet={`${car.url}.webp`} />
-              <img src={`${car.url}.jpg`} className={styles.img} loading="lazy" />
-            </picture>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {selection && (
+        <div className={styles.label}>
+          <h2>
+            Search results for : <strong>{selection}</strong>
+          </h2>
+        </div>
+      )}
+      <div className={styles.grid}>
+        {data?.map((car) => {
+          return (
+            <div key={car.id} className={styles.imgContainer}>
+              <picture>
+                <source srcSet={`${car.url}.webp`} />
+                <img src={`${car.url}.jpg`} className={styles.img} loading="lazy" />
+              </picture>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
